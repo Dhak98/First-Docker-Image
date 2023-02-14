4c18675809ebcb4006d3e7206a8696a7f2b85de6
dhak98 <dhakchanamoorthy23@gmail.com>
2023-02-14 21:44:58 +0530

    Just updates-1

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-def cap(s):
-    print(s.capitalize())
+import os
+from flask import Flask
+app = Flask(__name__)
 
-cap(s=input("Entert the string you want to Capital: "))
+@app.route('/')
+def method_name():
+    return "This is just a Sample docker image for learn how to build the image"
+
+if __name__ == "__main__":
+    app.run()
